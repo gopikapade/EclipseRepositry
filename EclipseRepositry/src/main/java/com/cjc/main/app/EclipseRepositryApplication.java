42791f2dae0be4001a66ede00642db7d5c726fea
gopikapade <kapadegopi.gk2022@gmail.com>
2023-05-03 16:22:57 +0530

    Intialisation

--- EclipseRepositry/src/main/java/com/cjc/main/app/EclipseRepositryApplication.java
+++ EclipseRepositry/src/main/java/com/cjc/main/app/EclipseRepositryApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EclipseRepositryApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Repsositry");
 		SpringApplication.run(EclipseRepositryApplication.class, args);
 	}
 
